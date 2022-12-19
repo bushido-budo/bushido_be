c1d592a6aaea50d1e95a91cf0116435cdd9f4553
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-12-19 10:03:14 -0700

    add relational infomation and validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates_presences_of :first_name
+  validates_presences_of :last_name
+  validates_presences_of :address
+  validates_presences_of :user_status
+  validates_uniqueness :email, case_sensitive: false
+  has_secure_password
+
   has_many :user_belt_ranks
+
+  before_save :downcase_email
+
+  def downcase_email
+    email&.downcase!
+  end
 end
