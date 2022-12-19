6841022c3a1f34e368598de762d96c5606b9cd43
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-12-19 12:27:31 -0700

    add all relationships to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,21 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :user_belt_ranks
+  has_many :belt_ranks, through: :user_belt_ranks
+  has_many :blocking_systems, through: :belt_ranks
+  has_many :blocks, through: :blocking_systems
+  has_many :combinations, through: :belt_ranks
+  has_many :grab_defenses, through: :belt_ranks
+  has_many :kata_forms, through: :belt_ranks
+  has_many :kata_skill_levels, through: :belt_ranks
+  has_many :kata_skills, through: :kata_skill_levels
+  has_many :kempo_techniques, through: :belt_ranks
+  has_many :club_techniques, through: :belt_ranks
+  has_many :kicks, through: :belt_ranks
+  has_many :foot_works, through: :belt_ranks
+  has_many :kick_defenses, through: :belt_ranks
+  has_many :skills, through: :belt_ranks
+  has_many :stances, through: :belt_ranks
+  has_many :kicks_of_the_four_tides, through: :belt_ranks
+  has_many :strikes, through: :belt_ranks
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_many :user_belt_ranks }
-  it { should validate_presence_of :first_name }
-  it { should validate_presence_of :last_name }
-  it { should validate_presence_of :user_status }
-  it { should validate_presence_of :email }
-  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
-  it { should have_secure_password }
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :user_status }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { should have_secure_password }
+  end
+
+  describe 'relationships' do
+    it { should have_many :user_belt_ranks }
+    it { should have_many :belt_ranks }
+    it { should have_many :blocking_systems }
+    it { should have_many :blocks }
+    it { should have_many :combinations }
+    it { should have_many :grab_defenses }
+    it { should have_many :kata_forms }
+    it { should have_many :kata_skill_levels }
+    it { should have_many :kata_skills }
+    it { should have_many :kempo_techniques }
+    it { should have_many :club_techniques }
+    it { should have_many :kicks }
+    it { should have_many :foot_works }
+    it { should have_many :kick_defenses }
+    it { should have_many :skills }
+    it { should have_many :stances }
+    it { should have_many :kicks_of_the_four_tides }
+    it { should have_many :strikes }
+  end
 end
