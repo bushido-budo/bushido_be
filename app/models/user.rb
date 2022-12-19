37ccff7072daac07b48e487051fc89a1621b39f3
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-12-19 11:51:55 -0700

    add validations for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :address
+  validates_presence_of :user_status
+  validates_presence_of :email
+  validates_uniqueness_of :email, case_sensitive: false
+  has_secure_password
+
   has_many :user_belt_ranks
 end
